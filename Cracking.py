b104bc6012e9b0e33ab5310f3b9840b52eeaca6d
DinisDimitris <d.dinisdimitris@gmail.com>
2022-10-03 16:56:50 +0100

    refactoring classes, python oop is p00p

--- Cracking.py
+++ Cracking.py
@@ -1,15 +1,22 @@
-from HashCracker import BruteForceHashCracker, HashCracker, DictHashCracker
+from HashCrackers.BruteForceHashCracker import BruteForceHashCracker
+from HashCrackers.DictHashCracker import DictHashCracker
+
+def Read(path):
+        fileObj = open(path, "r")
+        content = fileObj.read().splitlines()
+        fileObj.close()
+        return content
+
 
 if __name__ == '__main__':
-    hashes = HashCracker.Read('hashes.txt')
+    hashes = Read('hashes/hashes.txt')
 
     print ('Brute force hash cracker\n ---------------------')
     for hash in hashes:
         BruteForceHashCracker.AttemptBruteForce(hash)
 
-    
-    dictHashes = HashCracker.Read('dicthashes.txt')
-    passwords = HashCracker.Read('PasswordDictionary.txt')
+    dictHashes = Read('hashes/dicthashes.txt')
+    passwords = Read('hashes/PasswordDictionary.txt')
 
     print ('\nDictionary attack\n ---------------------')
     dictHashCracker = DictHashCracker(dictHashes, passwords)
@@ -17,4 +24,16 @@ if __name__ == '__main__':
     for dictHash in dictHashes:
         dictHashCracker.AttemptBruteForce(dictHash)
 
+    print ('\nSalted Dictionary attack\n ---------------------')
+    saltedHashes = [('915edb4d39ab6d260e3fb7269f5d9f8cfba3fdc998415298af3e6eb94a82e43e','27fb57e9'),
+    ('5ddce1dc316e7914ab6af64ef7c00d8b603fac32381db963d9359c3371a84b3a','b7875b4b'),
+    ('7e3b02bacd934245aa0cb3ea4d2b2f993a8681a650e38a63175374c28c4a7d0d','ec13ab35'),
+    ('d3136c0cb931acc938de13ed45926eb8764f9ea64af31be479be157480fd3014','29b49fce'),
+    ('3a9053a077383d11f5963ef0c66b38c7eb8331cdb03bbdcc0e5055307f67331b','acdabf8a'),
+    ('59c05d8d7b6d29279975141f7329cd77a5dc6942b036f9dfd30cbcb52c320cb4','64afe39d'),
+    ('c93802a2273a13c2b8378f98dda9f166783cbfce508aeaf570ad0b19a906b4d2','f0919683'),
+    ('e6a9713791c2ffeddbf6c6c395add47e1fc02ae1fa47febbbdfb694ed688ba61','081b2451'),
+    ('e6ec51a2ef933920ac1e6d3d8ba6ffac77fe94bfb79518b03cd9b94a14e97d3e','defb64a3'),
+    ('fbecd00c62b01135f9e588883e80f2710a354c0eb73a33a2c5ab5602cc85f6ad','017bb5b7')]
     
+
---
+++ HashCrackers/BruteForceHashCracker.py
@@ -0,0 +1,27 @@
+import hashlib
+from itertools import product
+
+class BruteForceHashCracker(object):
+
+    CHARS = 'abcdefghijklmnopqrstuvwxyz0123456789'
+    #adjust permutation depth so that memory doesnt blow up if ran locally and hash is not found
+    PERMUTATION_DEPTH = 5
+
+    @staticmethod
+    def words(letters):
+        for n in range(1, BruteForceHashCracker.PERMUTATION_DEPTH):
+           yield from map(''.join, product(letters, repeat=n))
+
+    @staticmethod
+    def AttemptBruteForce(hashToBeCracked):
+        for word in BruteForceHashCracker.words(BruteForceHashCracker.CHARS):
+                password = ''.join(word)
+                h = hashlib.new('sha256')
+                encoded = password.encode()
+                h.update(encoded)
+                hash = h.hexdigest()
+                if (hash == hashToBeCracked):   
+                    print("Hash: " + hashToBeCracked +  " has been cracked, password is: " + password)
+                    return
+
+        print('Hash: ' + hashToBeCracked + ' could not be found for permutation depth = ' + str(BruteForceHashCracker.PERMUTATION_DEPTH))
