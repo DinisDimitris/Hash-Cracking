5b34a47308721fb16f01d4af2ca484d9018e8469
DinisDimitris <d.dinisdimitris@gmail.com>
2022-10-03 15:56:37 +0100

    Adding dictionary attack hash cracking method

--- Cracking.py
+++ Cracking.py
@@ -1,7 +1,20 @@
-from HashCracker import BruteForceHashCracker
+from HashCracker import BruteForceHashCracker, HashCracker, DictHashCracker
 
 if __name__ == '__main__':
-    hashes = BruteForceHashCracker.ReadHashes('hashes.txt')
+    hashes = HashCracker.Read('hashes.txt')
 
+    print ('Brute force hash cracker\n ---------------------')
     for hash in hashes:
         BruteForceHashCracker.AttemptBruteForce(hash)
+
+    
+    dictHashes = HashCracker.Read('dicthashes.txt')
+    passwords = HashCracker.Read('PasswordDictionary.txt')
+
+    print ('\nDictionary attack\n ---------------------')
+    dictHashCracker = DictHashCracker(dictHashes, passwords)
+
+    for dictHash in dictHashes:
+        dictHashCracker.AttemptBruteForce(dictHash)
+
+    
--- HashCracker.py
+++ HashCracker.py
@@ -1,18 +1,21 @@
 import hashlib
 from itertools import product
 
+class HashCracker(object):
+
+    @staticmethod
+    def Read(path):
+        fileObj = open(path, "r")
+        content = fileObj.read().splitlines()
+        fileObj.close()
+        return content
+
+        
 class BruteForceHashCracker(object):
 
     CHARS = 'abcdefghijklmnopqrstuvwxyz0123456789'
     #adjust permutation depth so that memory doesnt blow up if ran locally
     PERMUTATION_DEPTH = 5
-
-    @staticmethod
-    def ReadHashes(path):
-        fileObj = open(path, "r")
-        content = fileObj.read().splitlines()
-        fileObj.close()
-        return content
 
     @staticmethod
     def words(letters):
@@ -32,3 +35,47 @@ class BruteForceHashCracker(object):
                     return
 
         print('Hash: ' + hashToBeCracked + ' could not be found for permutation depth = ' + str(BruteForceHashCracker.PERMUTATION_DEPTH))
+
+class DictHashCracker():
+
+    _hashDict = {}
+    _passwords = []
+
+    def __init__(self, hashes, passwords):
+        for hash in hashes:
+            self._hashDict.update({hash : ''})
+
+        self._passwords = passwords
+
+    def LookupHash(self,hash):
+        for hashKey in self._hashDict:
+            if (hash == hashKey):
+                if (self._hashDict[hashKey] != ''):
+                    return self._hashDict[hashKey]
+                
+                return ''
+        
+        raise Exception('Hash: ' + hash + ' has not been found in the hash dictionary.')
+
+    def AttemptBruteForce(self,hash):
+        lookupAttempt = self.LookupHash(hash) 
+
+        if  lookupAttempt != '':
+            return lookupAttempt
+        
+        for password in self._passwords:
+
+            h = hashlib.new('sha256')
+            encoded = password.encode()
+            h.update(encoded)
+            hashedPw = h.hexdigest()
+            if (hash == hashedPw):
+                print('Password for hash: ' + hash +  ' is: ' + password)
+                self._hashDict[hash] = password
+                return
+        
+        print ('Could not find password for hash: ' + hash)
+
+
+
+        
