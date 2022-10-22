7f1afcafb4716c9dd88118ffc8c747cb07a439c5
DinisDimitris <d.dinisdimitris@gmail.com>
2022-10-22 12:47:35 +0100

    rename

--- HashCrackers/BruteForceHashCracker.py
+++ HashCrackers/BruteForceHashCracker.py
@@ -5,12 +5,12 @@ class BruteForceHashCracker(object):
 
     CHARS = 'abcdefghijklmnopqrstuvwxyz0123456789'
     #adjust permutation depth so that memory doesnt blow up if ran locally and hash is not found
-    PERMUTATION_DEPTH = 20
+    CROSS_PROD_DEPTH = 20
 
     # yield here will return a generator instead of returning a list of PERMUTATION_DEPTH**len(CHARS)
     @staticmethod
     def words(letters):
-        for n in range(1, BruteForceHashCracker.PERMUTATION_DEPTH):
+        for n in range(1, BruteForceHashCracker.CROSS_PROD_DEPTH):
            yield from map(''.join, product(letters, repeat=n))
 
     @staticmethod
@@ -26,4 +26,4 @@ class BruteForceHashCracker(object):
                     print("Hash: " + hashToBeCracked +  " has been cracked, password is: " + password)
                     return password
 
-        print('Hash: ' + hashToBeCracked + ' could not be found for permutation depth = ' + str(BruteForceHashCracker.PERMUTATION_DEPTH))
+        print('Hash: ' + hashToBeCracked + ' could not be found for permutation depth = ' + str(BruteForceHashCracker.CROSS_PROD_DEPTH))
